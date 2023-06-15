50478b6b065e7d1f9ddf6b84998a9a7b2b9eb95d
Andreea S <stoian_am@yahoo.com>
2023-06-15 23:25:54 +0300

    UI Updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,28 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import ReactLoading from 'react-loading';
 
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBIcon,
+  MDBBtn,
+  MDBTypography,
+  MDBTextArea,
+  MDBCardHeader,
+} from "mdb-react-ui-kit";
+
 const localStorageKey = 'chatKey';
 
 function App() {
   const [message, setMessage] = useState("");
   const [chats, setChats] = useState([{
-    role: "user_msg", content: `Hi I'm your bot etc tec`
+    role: "user_msg", content: `Hi I'm your bot etc tec`, date: new Date()
   }]);
   const [isTyping, setIsTyping] = useState(false);
-  const ref = useRef(null);
+  const scrollRef = useRef(null);
 
   useEffect(() => {
     const key = localStorage.getItem(localStorageKey);
@@ -25,25 +38,12 @@ function App() {
     if (!message) return;
 
     let msgs = chats;
-    msgs.push({ role: "user", content: message });
-    msgs.push({ role: "loading", content: '' })
+    msgs.push({ role: "user", content: message, date: new Date() });
+    msgs.push({ role: "loading", content: '', date: null })
     setChats(msgs);
 
-    setTimeout(() => { ref.current.scrollTo(0, 10e6); setIsTyping(true); }, 200)
+    setTimeout(() => { scrollRef.current.scrollTo(0, 10e6); setIsTyping(true); }, 200)
     setMessage("");
-
-
-    // setTimeout(() => {
-    //   setChats((prevChats) => {
-    //     prevChats[prevChats.length - 1] = { role: "user_msg", content: `bot_${message}` };
-    //     localStorage.setItem(localStorageKey, JSON.stringify(prevChats));
-    //     return prevChats;
-    //   });
-    //   setIsTyping(false);
-    //   setTimeout(() => ref.current.scrollTo(0, 10e6), 500)
-    // }, 2000)
-
-
 
     fetch("http://localhost:5000/", {
       method: "POST",
@@ -58,63 +58,129 @@ function App() {
       .then((data) => {
         console.info(data);
         setChats((prevChats) => {
-          prevChats[prevChats.length - 1] = { role: "user_msg", content: data.response };
+          prevChats[prevChats.length - 1] = { role: "user_msg", content: data.response, date: new Date() };
           localStorage.setItem(localStorageKey, JSON.stringify(prevChats));
           return prevChats;
         });
         setIsTyping(false);
-        setTimeout(() => ref.current.scrollTo(0, 10e6), 500)
+        setTimeout(() => scrollRef.current.scrollTo(0, 10e6), 500)
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
+  const renderUserMessage = (chat) => (
+    <li className="d-flex justify-content-between mb-4">
+      <MDBCard className="w-100 mask-custom">
+        <MDBCardHeader
+          className="d-flex justify-content-between p-3"
+          style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
+        >
+          <p class="fw-bold mb-0">{chat.role}</p>
+          <p class="text-light small mb-0">
+            <MDBIcon far icon="clock" /> {new Date(chat.date).toLocaleString()}
+          </p>
+        </MDBCardHeader>
+        <MDBCardBody>
+          <p className="mb-0">
+            {chat?.content}
+          </p>
+        </MDBCardBody>
+      </MDBCard>
+      <img
+        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp"
+        alt="avatar"
+        className="rounded-circle d-flex align-self-start ms-3 shadow-1-strong"
+        width="60"
+      />
+    </li>
+  );
+
+  const renderBotMessage = (chat) => (
+    <li className="d-flex justify-content-between mb-4">
+      <img
+        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
+        alt="avatar"
+        className="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
+        width="60"
+      />
+      <MDBCard className="mask-custom" style={{ width: '100%' }}>
+        <MDBCardHeader
+          className="d-flex justify-content-between p-3"
+          style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
+        >
+          <p className="fw-bold mb-0">{chat.role}</p>
+          <p className="text-light small mb-0">
+            <MDBIcon far icon="clock" /> {new Date(chat.date).toLocaleString()}
+          </p>
+        </MDBCardHeader>
+        <MDBCardBody>
+          <p className="mb-0">
+            {chat?.content}
+          </p>
+        </MDBCardBody>
+      </MDBCard>
+    </li>
+  );
+
+  const onEnterPress = (e, message) => {
+    if (e.keyCode == 13 && e.shiftKey == false) {
+      e.preventDefault();
+      chat(e, message);
+    }
+  }
+
   return (
-    <main>
-      <h1>FullStack Chat AI Tutorial</h1>
-
-      <section
-        ref={(el) => { ref.current = el; }}
-        style={{
-          overflow: 'auto',
-          height: 550,
-          border: '4px solid black',
-          padding: 5,
-          marginBottom: 10,
-        }}>
-        {chats && chats.length
-          ? chats.map((chat, index) => {
-            if (chat.role === "loading") {
-              return <ReactLoading height={50} width={50} type={'bubbles'} />
-            }
-            return (
-              <p key={index} className={chat?.role === "user" ? "user_msg" : "bot_msg"}>
-                <span>
-                  <b>{chat?.role.toUpperCase()}</b>
-                </span>
-                <span>:</span>
-                <span>{chat?.content}</span>
-              </p>
-            )
-          })
-          : ""}
-      </section>
-
-      <form action="" onSubmit={(e) => chat(e, message)}>
-        <input
-          type="text"
-          name="message"
-          value={message}
-          placeholder="Type a message here and hit Enter..."
-          onChange={(e) => setMessage(e.target.value)}
-          style={{
-            borderRadius: 25,
-          }}
-          disabled={isTyping}
-        />
-      </form>
-    </main>
+    <MDBContainer fluid className="py-5 gradient-custom" style={{ width: '50%' }}>
+      <MDBRow>
+        <MDBCol md="12" lg="12" xl="12">
+          <MDBTypography listUnStyled className="text-white">
+            <div style={{ overflow: 'auto', height: 600 }} ref={(ref) => { scrollRef.current = ref; }}>
+              {chats && chats.length
+                ? chats.map((chat, index) => {
+                  if (chat.role === "loading") {
+                    return <ReactLoading height={50} width={50} type={'bubbles'} />
+                  }
+                  return (
+                    chat?.role === "user" ? renderUserMessage(chat) : renderBotMessage(chat)
+                  )
+                })
+                : ""}
+            </div>
+          </MDBTypography>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow>
+        <MDBCol md="12" lg="12" xl="12">
+          <MDBTypography listUnStyled className="text-white">
+            <li class="mb-3">
+              <div class="form-outline form-white">
+                <textarea
+                  class="form-control active"
+                  id="textAreaExample3"
+                  rows="4"
+                  style={{ height: 80 }}
+                  onChange={(e) => setMessage(e.target.value)}
+                  value={message}
+                  disabled={isTyping}
+                  onKeyDown={(e) => onEnterPress(e, message)}
+                />
+                <label class="form-label" for="textAreaExample3" style={{ marginLeft: 0 }}>Message</label>
+                <div class="form-notch"><div class="form-notch-leading" style={{ width: 9 }}>
+                </div>
+                  <div class="form-notch-middle" style={{ width: 60 }} />
+                  <div class="form-notch-trailing" />
+                </div>
+              </div>
+            </li>
+            <MDBBtn color="light" size="lg" rounded className="float-end" onClick={(e) => chat(e, message)}>
+              Send
+            </MDBBtn>
+          </MDBTypography>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer >
   );
 }
 
